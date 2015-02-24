42ea85c40acddae058c1ddb9250fba79e2321b43
Sven Panne <svenpanne@gmail.com>
2015-02-24 11:31:20 +0100

    More SB6 framework.

--- src/SB6/SB6.hs
+++ src/SB6/SB6.hs
@@ -1,28 +1,35 @@
+{-# LANGUAGE CPP #-}
 module SB6 (
   Application(..), app,
   APPINFO(..), appInfo,
   run
 ) where
 
-import Graphics.Rendering.OpenGL.GL
+import Control.Monad
+import Data.Time.Clock
+import System.IO
 import Graphics.UI.GLUT as GLUT
 
 data Application = Application {
   init :: IO APPINFO,
-  render :: Double -> IO ()
-  }
+  startup :: IO (),
+  render :: Double -> IO (),
+  shutdown :: IO (),
+  onResize :: Size -> IO (),
+  onKey :: Either SpecialKey Char -> KeyState -> IO (),
+  onMouseButton :: MouseButton -> KeyState -> IO (),
+  onMouseMove :: Position -> IO () }
 
 app :: Application
 app = Application {
-  SB6.init = defaultInit,
-  render = defaultRender
-  }
-
-defaultInit :: IO APPINFO
-defaultInit = return appInfo
-
-defaultRender :: Double -> IO ()
-defaultRender _currentTime = return ()
+  SB6.init = return appInfo,
+  startup = return (),
+  render = \_currentTime -> return (),
+  shutdown = return (),
+  onResize = \_size -> return (),
+  onKey = \_key _keyState -> return (),
+  onMouseButton = \_mouseButton _keyState -> return (),
+  onMouseMove = \_position -> return () }
 
 data APPINFO = APPINFO {
   title :: String,
@@ -33,41 +40,98 @@ data APPINFO = APPINFO {
   vsync :: Bool,
   cursor :: Bool,
   stereo :: Bool,
-  debug :: Bool
-  }
+  debug :: Bool }
 
 appInfo :: APPINFO
 appInfo = APPINFO {
   title = "SuperBible6 Example",
   SB6.windowSize  = Size 800 600,
-  -- TODO: For __APPLE__ => (3, 2)
+#if DARWIN
+  version = (3, 2),
+#else
   version = (4, 3),
+#endif
   SB6.samples = 0,
   fullscreen  = False,
   vsync  = False,
   SB6.cursor  = True,
   SB6.stereo  = False,
-  -- TODO: For _DEBUG => True
+#if DEBUG
+  debug  = True
+#else
   debug  = False
+#endif
   }
 
 run :: Application -> IO ()
 run theApp = do
-  (progName, _args) <- getArgsAndInitialize
+  startTime <- getCurrentTime
+  void getArgsAndInitialize
   theAppInfo <- SB6.init theApp
-  initialDisplayMode $= [ DoubleBuffered ]
-  initialWindowSize $= SB6.windowSize theAppInfo
-  initialContextVersion $= version theAppInfo
-  initialContextProfile $= [ CoreProfile ]
-  initialContextFlags $= [ ForwardCompatibleContext ] ++
-    (if debug theAppInfo then [ DebugContext ] else [] )
-  _ <- createWindow (title theAppInfo)
-  displayCallback $= display (render theApp)
-  mainLoop
 
-display :: (Double -> IO ()) -> IO ()
-display theRender = do
-  t <- get elapsedTime
-  theRender (fromIntegral t / 1000)
+  let numOpt f fld = opt (f . fromIntegral . fld $ theAppInfo) ((> 0) . fld)
+      opt val predicate = if predicate theAppInfo then [val] else []
+      width (Size w _) = w
+      height (Size _ h) = h
+
+  initialDisplayMode $=
+    [RGBAMode, WithDepthBuffer, DoubleBuffered] ++
+    numOpt WithSamplesPerPixel SB6.samples ++
+    opt Stereoscopic SB6.stereo
+  initialContextVersion $= version theAppInfo
+  initialContextProfile $= [CoreProfile]
+  initialContextFlags $= [ForwardCompatibleContext]
+#if DEBUG
+    ++ [DebugContext]
+#endif
+
+  if fullscreen theAppInfo
+    then do
+      gameModeCapabilities $=
+        [Where' GameModeBitsPerPlane IsEqualTo 32] ++
+        numOpt (Where' GameModeWidth IsEqualTo) (width . SB6.windowSize) ++
+        numOpt (Where' GameModeHeight IsEqualTo) (height . SB6.windowSize)
+      void enterGameMode
+      windowTitle $= title theAppInfo
+    else do
+      initialWindowSize $= SB6.windowSize theAppInfo
+      void . createWindow . title $ theAppInfo
+
+  unless (SB6.cursor theAppInfo) $
+    GLUT.cursor $= None
+
+  displayCallback $= display theApp startTime
+  reshapeCallback $= Just (onResize theApp)
+  keyboardMouseCallback $= Just (keyboardMouse theApp)
+  motionCallback $= Just (onMouseMove theApp)
+  passiveMotionCallback $= Just (onMouseMove theApp)
+
+  -- TODO: Setup debug message callback
+
+#if DEBUG
+  forM_ [("VENDOR", vendor),
+         ("VERSION", glVersion),
+         ("RENDERER", renderer)] $ \(name, var) -> do
+    val <- get var
+    hPutStrLn stderr (name ++ ": " ++ val)
+#endif
+
+  startup theApp
+  mainLoop
+  shutdown theApp
+  gma <- get gameModeActive
+  when gma leaveGameMode
+
+display :: Application -> UTCTime -> DisplayCallback
+display theApp startTime = do
+  currentTime <- getCurrentTime
+  render theApp $ realToFrac (currentTime `diffUTCTime` startTime)
   swapBuffers
   postRedisplay Nothing
+
+keyboardMouse :: Application -> KeyboardMouseCallback
+keyboardMouse theApp key keyState _modifiers _position =
+  case key of
+    Char c -> onKey theApp (Right c) keyState
+    SpecialKey k -> onKey theApp (Left k) keyState
+    MouseButton b -> onMouseButton theApp b keyState
--- src/SimpleClear/SimpleClear.hs
+++ src/SimpleClear/SimpleClear.hs
@@ -2,24 +2,20 @@
 
 module Main ( main ) where
 
-import Foreign.Marshal.Utils
+import Foreign.Marshal.Array
 import Foreign.Ptr
-import Graphics.Rendering.OpenGL.GL.VertexSpec
 import Graphics.Rendering.OpenGL.Raw.Core43
 import SB6
 
 init :: IO APPINFO
-init = return $ appInfo {
-  title = "OpenGL SuperBible - Simple Clear"
-  }
+init = return $ appInfo { title = "OpenGL SuperBible - Simple Clear" }
 
 render :: Double -> IO ()
 render _currentTime =
-  with (Color4 1 0 0 1 :: Color4 GLfloat) $ \red ->
-    glClearBufferfv gl_COLOR 0 (castPtr red)
+  withArray ([1, 0, 0, 1] :: [GLfloat]) $
+    glClearBufferfv gl_COLOR 0 . castPtr
 
 main :: IO ()
-main = run $ Application {
+main = run $ app {
   SB6.init = Main.init,
-  SB6.render = Main.render
-  }
+  SB6.render = Main.render }
